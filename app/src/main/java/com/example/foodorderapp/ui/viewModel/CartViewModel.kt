26798cc9a672b0b2ad2f09f035be0e54f52c47d9
furkanayyildiz <furkan199905@gmail.com>
2023-10-18 22:04:49 +0300

    cart card view added

---
+++ app/src/main/java/com/example/foodorderapp/ui/viewModel/CartViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.foodorderapp.ui.viewModel
+
+import androidx.lifecycle.ViewModel
+import com.example.foodorderapp.data.repository.FoodRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CartViewModel @Inject constructor(var foodRepository: FoodRepository) : ViewModel() {
+
+}
